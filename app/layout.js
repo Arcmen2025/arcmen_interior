7bc8513017ecfed294b015353f9705e8169bad80
Ajin.T.S <149351487+Ajints@users.noreply.github.com>
2025-08-12 16:31:05 +0530

    resloved the build error

--- app/layout.js
+++ app/layout.js
@@ -86,7 +86,7 @@ fbq('track', 'PageView');
                     }}
                 />
                 <noscript>
-                    <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1002700818509764&ev=PageView&noscript=1" />
+                    <img height="1" width="1" style={{ display: "none" }} src="https://www.facebook.com/tr?id=1002700818509764&ev=PageView&noscript=1" />
                 </noscript>
             </head>
             <body>
